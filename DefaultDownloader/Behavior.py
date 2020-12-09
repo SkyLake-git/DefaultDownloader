bc40c6b3296f19fe749c1b70cf4623da72204a20
SkyLake-git <70795425+SkyLake-git@users.noreply.github.com>
2020-12-09 18:23:20 +0900

    v1.16.200 対応

---
+++ DefaultDownloader/Behavior.py
@@ -0,0 +1,29 @@
+import urllib.request
+import zipfile
+import os
+import sys
+import time
+
+def load(block_count, block_size, total_size):
+	percentage = 100.0 * block_count * block_size / total_size
+	total_size_kb = total_size / 1024
+	if percentage > 100:
+		percentage = 100
+	sys.stdout.write(f'\rダウンロード >> {percentage:.2f}% ({total_size_kb:.0f}kb)')
+
+DLname = 'Behavior'
+version = '1.16.200' #ダウンロードに失敗した場合はこの値を最新バージョンに変更してください
+resourceUrl = f'https://meedownloads.blob.core.windows.net/worlds/Vanilla_{DLname}_Pack_{version}.zip'
+
+os.makedirs(DLname, exist_ok=True)
+urllib.request.urlretrieve(resourceUrl, f'{DLname}/{DLname}.zip', load)
+sys.stdout.write('\nファイルの解凍 >> 進行中')
+with zipfile.ZipFile(f'{DLname}/{DLname}.zip') as zf:
+	zf.extractall(f'{DLname}/')
+sys.stdout.write('\rファイルの解凍 >> 完了   ')
+sys.stdout.write('\nファイルの削除 >> 進行中')
+os.remove(f'{DLname}/{DLname}.zip')
+sys.stdout.write('\rファイルの削除 >> 完了    ')
+
+time.sleep(3)
+sys.exit()
